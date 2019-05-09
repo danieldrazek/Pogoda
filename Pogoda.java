69924a8d183793f7c078c990ca53f4ef745e0afa
danieldrazek <daniel_2000@o2.pl>
2019-05-09 12:35:42 +0100

    add file

---
+++ Pogoda.java
@@ -0,0 +1,23 @@
+
+package pogoda;
+
+public class Pogoda {
+
+    public static void main(String[] args) {
+        
+        String winterWeather = "Snow is falling in winter";
+        String summerWeather = "In the Summer the sun is shining";
+        String autumnWeather = "It is raining in the autumn";
+        
+        int winterWeatherLength = winterWeather.length();
+        int summerWeatherLength = summerWeather.length();
+        int autumnWeatherLength = autumnWeather.length();
+        int allSentenceLength = winterWeatherLength + autumnWeatherLength + summerWeatherLength;
+        
+        System.out.println("winterWeatherLength is " + winterWeatherLength + " characters");
+        System.out.println("summerWeatherLength is " + summerWeatherLength + " characters");
+        System.out.println("autumnWeatherLength is " + autumnWeatherLength + " characters");
+        System.out.println("allSentenceLength is " + allSentenceLength + " characters");
+    }
+    
+}
